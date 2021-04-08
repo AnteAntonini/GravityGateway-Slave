5c649836e012d3f3ea090071f5e6f648e5d43633
AnteAntonini <anteantonini.mtk@hotmail.com>
2021-04-08 10:20:27 +0200

    slave commit

---
+++ src/components/Slave.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <h1>Slave</h1>
+    </div>
+</template>
+
+
+
+<script>
+import Gateway from '@nsoft/seven-gravity-gateway/slave';
+
+
+let slave = Gateway({
+    slaveId : 'GatewayPlayground',
+    data : {test: "hello from slave"},
+    load: function(data) {
+      console.log('slave load', data)
+    },
+    allowedOrigins: '*',
+    debug : false
+});
+
+
+slave.emit({
+  action : 'Betslip.Add',
+}, '*');
+
+
+export default {
+  name: 'Slave',
+  data() {
+    return {
+    
+    }
+  }
+
+}
+
+</script>
