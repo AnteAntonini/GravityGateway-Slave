1656b5844195a5dca665c266c86fc0ffb03bbc20
AnteAntonini <anteantonini.mtk@hotmail.com>
2021-04-09 13:32:52 +0200

    send event from slave

--- src/components/Slave.vue
+++ src/components/Slave.vue
@@ -1,6 +1,7 @@
 <template>
     <div>
-        <h1>Slave</h1>
+      <h1>Slave</h1>
+      <button @click="msgToMaster">Send to Master</button>
     </div>
 </template>
 
@@ -13,27 +14,24 @@ import Gateway from '@nsoft/seven-gravity-gateway/slave';
 let slave = Gateway({
     slaveId : 'GatewayPlayground',
     data : {test: "hello from slave"},
-    load: function(data) {
-      console.log('slave load', data)
-    },
+    load: function() {},
     allowedOrigins: '*',
-    debug : false
+    debug : true,
 });
 
 
-slave.emit({
-  action : 'Betslip.Add',
-}, '*');
-
+slave.on('helloSlave', (event) => console.log(event.data))
 
 export default {
   name: 'Slave',
-  data() {
-    return {
-    
+  methods: {
+    msgToMaster() {
+      slave.emit({
+       data: {slaveMessage: 'slave message to master'},
+       action : 'helloMaster',
+    }, '*');
     }
   }
-
 }
 
 </script>
